8f1691935d6b7a6eb2cbfe6f1b155f03de0b4397
Ky <heovang097@gmail.com>
2021-06-30 20:39:17 +0700

    dump SQL

--- MySQL file/ky/fwp.sql
+++ MySQL file/ky/fwp.sql
@@ -11,11 +11,30 @@
  Target Server Version : 100419
  File Encoding         : 65001
 
- Date: 30/06/2021 17:32:37
+ Date: 30/06/2021 20:38:56
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for articles
+-- ----------------------------
+DROP TABLE IF EXISTS `articles`;
+CREATE TABLE `articles`  (
+  `articleID` int NOT NULL AUTO_INCREMENT,
+  `articleName` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `tag` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `avater` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`articleID`) USING BTREE,
+  CONSTRAINT `CatID` FOREIGN KEY (`articleID`) REFERENCES `categories` (`CatID`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of articles
+-- ----------------------------
+INSERT INTO `articles` VALUES (1, 'Buôn bán nông sản', NULL, 'https://i1-vnexpress.vnecdn.net/2021/06/30/203361413-529564385145353-6575-7676-6568-1625045931.jpg?w=1020&h=0&q=100&dpr=1&fit=crop&s=UcnmdKvtO_J5zSfUnOE5YA');
+INSERT INTO `articles` VALUES (2, 'Buôn bán thủy sản', NULL, NULL);
 
 -- ----------------------------
 -- Table structure for categories
@@ -25,8 +44,9 @@ CREATE TABLE `categories`  (
   `CatID` int NOT NULL AUTO_INCREMENT,
   `CatName` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `CatLink` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  PRIMARY KEY (`CatID`, `CatName`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 12 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+  PRIMARY KEY (`CatID`, `CatName`) USING BTREE,
+  INDEX `CatID`(`CatID`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 13 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of categories
@@ -36,6 +56,7 @@ INSERT INTO `categories` VALUES (2, 'Kinh doanh', 'kinhdoanh');
 INSERT INTO `categories` VALUES (3, 'Nông nghiệp', 'nongnghiep');
 INSERT INTO `categories` VALUES (4, 'Giáo dục', 'giaoduc');
 INSERT INTO `categories` VALUES (5, 'Y tế', 'yte');
+INSERT INTO `categories` VALUES (12, 'Quốc phòng', 'quocphong');
 
 -- ----------------------------
 -- Table structure for user
@@ -47,12 +68,13 @@ CREATE TABLE `user`  (
   `emailAddress` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `dob` date NULL DEFAULT NULL,
   PRIMARY KEY (`userID`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
 INSERT INTO `user` VALUES (1, 'Heovang097', 'heovang097@gmail.com', '2000-03-13');
 INSERT INTO `user` VALUES (2, 'Heovang098', 'heovang098@gmail.com', '2001-03-13');
+INSERT INTO `user` VALUES (3, 'Heovang099', NULL, NULL);
 
 SET FOREIGN_KEY_CHECKS = 1;
