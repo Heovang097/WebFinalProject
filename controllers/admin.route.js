885d4f15e2bc90c73ccdb33b8319bae060e499cb
Ky <heovang097@gmail.com>
2021-07-25 21:40:31 +0700

    CRUD Branch

--- controllers/admin.route.js
+++ controllers/admin.route.js
@@ -1,34 +1,12 @@
 const express = require("express");
 const articleModel = require("../models/article.model");
 const moment = require('moment');
+const categoryModel = require("../models/category.model");
  
-
 const router = express.Router();
 
-router.get("/tags", async function(req, res) {
-    const articleList = await articleModel.allByEditorID(req.session.authUser.UserID);
-    // console.log(`====== Article List by Editor ID: ${req.session.authUser.UserID} ======`);
-    // console.log(articleList);
-    // console.log(`================================`);
-    res.render("vwEditor/list", {
-        articleList: articleList,
-        categories: res.locals.lcCategories,
-    })
-});
-
-router.post("/deny/:id", async function(req, res) {
-    const articleID = req.params.id;
-    const reason = req.body.Reason;
-    await articleModel.deny(articleID, reason);
-    res.redirect('../list');
-})
-
-router.post("/approve/:id", async function(req, res) {
-    console.log("Editor approve request: " + req.body);
-    const dateOfPublish = moment(req.body.DateOfPublish, 'DD/MM/YYYY hh:mm').format('YYYY-MM-DD hh:mm:ss');
-    const tag = req.body.Tag
-    const articleID = req.params.id;
-    await articleModel.approve(articleID, tag, dateOfPublish);
-    res.redirect('../list');
-})
+router.use("/categories", require("./admin/categories.route"));
+router.use("/tags", require("./admin/tags.route.js"));
+router.use("/articles", require("./admin/articles.route"));
+router.use("/users", require("./admin/users.route"));
 module.exports = router
